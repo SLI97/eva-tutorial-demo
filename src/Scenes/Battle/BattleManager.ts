b3a53d47b076b3ff9657960fbd53e92346df9816
liangchaogui <liangchaogui@bytedance.com>
2022-03-04 18:24:21 +0800

    p12

--- src/Scenes/Battle/BattleManager.ts
+++ src/Scenes/Battle/BattleManager.ts
@@ -4,22 +4,34 @@ import levels from '../../Levels';
 import { SCREEN_HEIGHT, SCREEN_WIDTH } from '../../index';
 import { TILE_HEIGHT, TILE_WIDTH } from './GameObjects/Tile';
 import DataManager from '../../Runtime/DataManager';
+import EventManager from '../../Runtime/EventManager';
+import { EVENT_ENUM } from '../../Enums';
 
 export default class BattleManager extends Component {
   static componentName = 'BattleManager'; // 设置组件的名字
 
   init() {
+    EventManager.Instance.on(EVENT_ENUM.NEXT_LEVEL, this.nextLevel, this);
     this.initLevel();
   }
 
   initLevel() {
+    this.clearLevel();
     const { levelIndex } = DataManager.Instance;
     const level = levels[`level${levelIndex}`];
     DataManager.Instance.mapInfo = level.mapInfo;
-    DataManager.Instance.mapRowCount = level.mapInfo[0].length;
-    DataManager.Instance.mapColumnCount = level.mapInfo.length;
+    DataManager.Instance.mapRowCount = level.mapInfo.length;
+    DataManager.Instance.mapColumnCount = level.mapInfo[0].length;
 
     this.generateTileMap();
+  }
+
+  clearLevel() {
+    this.gameObject.transform.children.forEach(({ gameObject }) => {
+      gameObject.destroy();
+    });
+
+    DataManager.Instance.reset();
   }
 
   generateTileMap() {
@@ -33,5 +45,10 @@ export default class BattleManager extends Component {
     const disY = (SCREEN_HEIGHT - TILE_HEIGHT * mapColumnCount) / 2 - 50;
     this.gameObject.transform.position.x = disX;
     this.gameObject.transform.position.y = disY;
+  }
+
+  nextLevel() {
+    DataManager.Instance.levelIndex++;
+    this.initLevel();
   }
 }
